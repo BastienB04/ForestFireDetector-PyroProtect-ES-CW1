a73c3d194f3a8f700508c34f1dfb3e53f9b30fd4
aaa <abdal.alkilani@gmail.com>
2023-01-31 16:35:40 +0000

    chat init

--- frontend/api/init.js
+++ frontend/api/init.js
@@ -31,3 +31,49 @@ const domContainer = document.querySelector('#test');
 const root = ReactDOM.createRoot(domContainer);
 console.log(e(Map_));
 root.render(e(Map_));
+
+// const e = React.createElement;
+// const HOST = 'http://localhost:8080';
+
+// class Map_ extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             data: []
+//         };
+//     }
+
+//     componentDidMount() {
+//         fetch(HOST + '/api/initData')
+//             .then(res => res.json())
+//             .then(data => {
+//                 this.setState({ data });
+//             })
+//             .catch(error => console.error(error));
+//     }
+
+//     render() {
+//         const { data } = this.state;
+//         const result = [];
+//         for (let i = 0; i < data.rowSize; i++) {
+//             for (let j = 0; j < data.columnSize; j++) {
+//                 result.push(
+//                     e(
+//                         'div',
+//                         { key: `grid-${i}-${j}` },
+//                         'succ'
+//                     )
+//                 );
+//             }
+//         }
+//         return result;
+//     }
+// }
+
+// const domContainer = document.querySelector('#test');
+// if (domContainer) {
+//     const root = ReactDOM.createRoot(domContainer);
+//     root.render(e(Map_));
+// } else {
+//     console.error("Element with id 'test' not found in the HTML");
+// }
