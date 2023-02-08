5b9427db1caf47981b0d4be2ab4a4a7c979ee469
aaa <abdal.alkilani@gmail.com>
2023-02-08 16:12:03 +0000

    testing db

--- frontend/test.js
+++ frontend/test.js
@@ -2,3 +2,16 @@ const { Database } = require('./database');
 
 const database = new Database()
 
+test('addData should add data to the database', async () => {
+  const sensorId = 'testSensor1';
+  const data = {
+      temperature: 25,
+      humidity: 50
+  };
+
+  await database.addData(sensorId, data);
+
+  const result = await database.getData(sensorId);
+  expect(result[0].sensorId).toBe(sensorId);
+  expect(result[0].data).toEqual(data);
+});
