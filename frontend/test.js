e0c899a3623d72731df948ede4652a1d060c44d0
aaa <abdal.alkilani@gmail.com>
2023-02-08 15:22:19 +0000

    Database communication start

--- frontend/test.js
+++ frontend/test.js
@@ -1,26 +1,4 @@
-'use strict';
+const { Database } = require('./database');
 
-const e = React.createElement;
+const database = new Database()
 
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
-}
-
-const domContainer = document.querySelector('#test');
-const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
