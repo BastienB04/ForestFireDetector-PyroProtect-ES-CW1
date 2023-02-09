77f2586d6fa9b7ab440cedabbbde2608f39ccde6
lucjones02 <lucjones@dyn3152-177.wlan.ic.ac.uk>
2023-02-09 21:03:22 +0000

    renamed fronted folder to server

---
+++ server/processing/Station.ts
@@ -0,0 +1,10 @@
+class Station {
+    private temperature: number; // Celsius
+    private relativeHumidity: number; // percentage
+    private windSpeed:number; // km/h
+    private precipitation: number; // mm
+}
+
+class FWIStation extends Station{
+    
+}
