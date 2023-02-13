781188632502b9fe2fbb8c05d9b59de4187862c3
aaa <abdal.alkilani@gmail.com>
2023-02-13 18:07:52 +0000

    smol

--- server/src/Google_maps.js
+++ server/src/Google_maps.js
@@ -1,28 +1,23 @@
-
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 51.4988, lng: 0.1749},
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 51.4988, lng: 0.1749 },
     zoom: 8
   });
 
-  var customOverlay = new google.maps.OverlayView();
-  customOverlay.onAdd = function() {
-    var div = document.createElement('div');
+  const customOverlay = new google.maps.OverlayView();
+  customOverlay.onAdd = function () {
+    const div = document.createElement('div');
     div.id = 'grid';
+    div.style.display = "flex";
+    div.style.alignItems = "center";
     div.style.justifyContent = "center";
     div.style.position = "absolute";
     div.style.width = "100%";
     div.style.height = "100%";
 
-
     ReactDOM.render(<Map_ />, div);
     this.getPanes().floatPane.appendChild(div);
   };
+
   customOverlay.setMap(map);
-  customOverlay.position(new google.maps.LatLng(51.4988, 0.1749));
 };
-
-
-
-
-
