eb5a7191da728c5dde0655e7d5f994cb81046fbc
Zsombito <klapper.zsombor@gmail.com>
2023-02-15 14:48:42 +0000

    Update Google_maps.js

--- server/src/Google_maps.js
+++ server/src/Google_maps.js
@@ -62,7 +62,7 @@ function initMap() {
       if (this.div) {
         this.div.style.left = sw.x + "px";
         this.div.style.top = sw.y + "px"; // Change to use ne.y instead of sw.y
-        var factor = Math.pow(2, zoomLevel-7);
+        var factor = 1 /  Math.pow(2, zoomLevel-7);
         this.div.style.width = (100 *factor) + "%";
         this.div.style.he = (100 *factor) + "%";
         //this.div.style.width = ne.x - sw.x + "px";
