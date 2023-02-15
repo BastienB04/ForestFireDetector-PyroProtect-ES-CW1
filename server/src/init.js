44c820a47bbce758001c0d89f028c58cbbe3fd59
Zsombito <klapper.zsombor@gmail.com>
2023-02-15 13:18:27 +0000

    Update init.js

--- server/src/init.js
+++ server/src/init.js
@@ -67,7 +67,6 @@ class Map_ extends React.Component {
     render() {
         const { gridSize, grid } = this.state;
         const el1 = document.getElementById('grid');
-        el1.setAttribute('id', 'grid');
         el1.style.display = 'grid';
         el1.style.gridTemplateColumns = templateConcat(gridSize, '40px');
         el1.style.gridTemplateRows = templateConcat(gridSize, '40px');
