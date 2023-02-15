c80583a57b788cb50464bd35bcf3c44232d6caae
Zsombito <klapper.zsombor@gmail.com>
2023-02-15 11:20:53 +0000

    Try

--- server/src/Google_maps.js
+++ server/src/Google_maps.js
@@ -48,8 +48,8 @@ function initMap() {
       const ne = overlayProjection.fromLatLngToContainerPixel(this.bounds.getNorthEast());
   
       if (this.div) {
-        this.div.style.left = sw.x + "px";
-        this.div.style.top = ne.y + "px"; // Change to use ne.y instead of sw.y
+        this.div.style.left = (ne.x- sw.x) + "px";
+        this.div.style.top = (sw.y - ne.y) + "px"; // Change to use ne.y instead of sw.y
         this.div.style.width = ne.x - sw.x + "px";
         this.div.style.height = sw.y - ne.y + "px"; // Change to use sw.y - ne.y instead of ne.y - sw.y
       }
