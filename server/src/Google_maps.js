04801b16bc6e9405517dba6e15840b608c1a94dd
aaa <abdal.alkilani@gmail.com>
2023-02-15 14:37:41 +0000

    hola mami

--- server/src/Google_maps.js
+++ server/src/Google_maps.js
@@ -1,112 +1,81 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 // import Map_ from './Map_';
-function templateConcat(n, str){
-  res = str
-  for(i=0; i<n-1; i++){
-      res+= " " + str
-  }
-  return res;
-}
-
-let map;
-let zoomLevel = 7;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+  const map = new google.maps.Map(document.getElementById('map'), {
     zoom: 7,
     center: { lat: 51.4988, lng: 0.1349 },
     mapTypeId: "satellite",
   });
-
   const bounds = new google.maps.LatLngBounds(
     new google.maps.LatLng(51.4488, 0.1049),
     new google.maps.LatLng(51.5488, 0.1649)
   );
 
   class CustomOverlay extends google.maps.OverlayView {
-    constructor(bounds) {
+    constructor(bounds, mapDiv) {
       super();
       this.bounds = bounds;
-      this.div = null;
+      this.mapDiv = mapDiv;
+      this.setMap(map);
     }
-  
+
     onAdd() {
-      this.div = document.createElement('div');
-      this.div.style.border = "none";
+      this.div = document.createElement("div");
+      this.div.style.borderStyle = "none";
       this.div.style.borderWidth = "0px";
       this.div.style.position = "absolute";
       this.div.style.width = "100%";
       this.div.style.height = "100%";
-      this.div.style.display = 'grid';
-      this.div.style.gridTemplateColumns = templateConcat(10, '40px');
-      this.div.style.gridTemplateRows = templateConcat(10, '40px');
-
+      this.mapDiv.appendChild(this.div);
       ReactDOM.render(<Map_ />, this.div);
-      const panes = this.getPanes();
-      panes.overlayLayer.appendChild(this.div);
-      this.div.addEventListener('click', (event) => {
-        // check if the clicked element is a dot
-        if (event.target.classList.contains('dot')) {
-          // navigate to the href specified in the a tag
-          window.location.href = event.target.parentNode.href;
-        }
-      });
-      this.div.addEventListener("click", this.handleOverlayClick.bind(this));
     }
-  
+
+    draw() {
+      const overlayProjection = this.getProjection();
+      const sw = overlayProjection.fromLatLngToDivPixel(this.bounds.getSouthWest());
+      const ne = overlayProjection.fromLatLngToDivPixel(this.bounds.getNorthEast());
+      const divWidth = ne.x - sw.x;
+      const divHeight = ne.y - sw.y;
+      const divCenterX = (ne.x + sw.x) / 2;
+      const divCenterY = (ne.y + sw.y) / 2;
+
+      if (this.div) {
+        // set the position of the div
+        this.div.style.left = divCenterX - divWidth / 2 + "px";
+        this.div.style.top = divCenterY - divHeight / 2 + "px";
+        // set the size of the div
+        this.div.style.width = divWidth + "px";
+        this.div.style.height = divHeight + "px";
+      }
+    }
+
     onRemove() {
       this.div.parentNode.removeChild(this.div);
       this.div = null;
     }
-    
-    draw() {
-      const overlayProjection = this.getProjection();
-      if (!overlayProjection) {
-        return;
-      }
   
-      const sw = overlayProjection.fromLatLngToDivPixel(this.bounds.getSouthWest());
-      const ne = overlayProjection.fromLatLngToDivPixel(this.bounds.getNorthEast());
-  
+    hide() {
       if (this.div) {
-        this.div.style.left = sw.x + "px";
-        this.div.style.top = sw.y + "px"; // Change to use ne.y instead of sw.y
-        this.div.style.width = (100 / (zoomLevel-7)) + "%";
-        this.div.style.he = (100 / (zoomLevel-7)) + "%";
-        //this.div.style.width = ne.x - sw.x + "px";
-        //this.div.style.height = sw.y - ne.y + "px"; // Change to use sw.y - ne.y instead of ne.y - sw.y
+        this.div.style.visibility = "hidden";
       }
-      google.maps.event.addListener(map, "zoom_changed", function () {
-        // Get the current zoom level
-         zoomLevel = map.getZoom();
-         console.log(zoomLevel);
-      });
     }
-    handleOverlayClick(event) {
-      event.preventDefault();
-      const target = event.target;
-      if (target.tagName === "A") {
-        const href = target.getAttribute("href");
-        if (href) {
-          window.location.href = href;
-        }
+  
+    show() {
+      if (this.div) {
+        this.div.style.visibility = "visible";
       }
     }
   }
   
-  
-
   const customOverlay = new CustomOverlay(bounds);
   customOverlay.setMap(map);
 }
-
-
 // function initMap() {
 //   const map = new google.maps.Map(document.getElementById('map'), {
 //     center: { lat: 51.4988, lng: 0.1749 },
 //     zoom: 8
 //   });
-
 //   const customOverlay = new google.maps.OverlayView();
 //   customOverlay.onAdd = function () {
 //     const div = document.createElement('div');
@@ -117,10 +86,3 @@ function initMap() {
 //     div.style.position = "absolute";
 //     div.style.width = "100%";
 //     div.style.height = "100%";
-
-//     ReactDOM.render(<Map_ />, div);
-//     this.getPanes().floatPane.appendChild(div);
-//   };
-
-//   customOverlay.setMap(map);
-// };
