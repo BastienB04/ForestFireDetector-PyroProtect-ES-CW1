4bc10e724b153742443b0b206726572794a0fcc9
Zsombito <klapper.zsombor@gmail.com>
2023-02-15 19:31:48 +0000

    Would this help?

--- server/src/init.js
+++ server/src/init.js
@@ -31,7 +31,7 @@ class Map_ extends React.Component {
             heatMap: []
         };
     }
-    fetchHeatMap()
+    async fetchHeatMap()
     {
         fetch(HOST + '/api/heatMap').then((res) => {
             return res.json();
