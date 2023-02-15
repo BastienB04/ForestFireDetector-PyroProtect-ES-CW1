81c43d883803cd438433156abcffa2785fdf080e
aaa <abdal.alkilani@gmail.com>
2023-02-15 15:15:31 +0000

    update coordinates marbke arch

--- server/src/Google_maps.js
+++ server/src/Google_maps.js
@@ -14,13 +14,13 @@ let zoomLevel = 16;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
     zoom: 16,
-    center: { lat: 51.4988, lng: -0.1349 },
+    center: { lat: 51.5088, lng: -0.1749 },
     mapTypeId: "satellite",
   });
 
   const bounds = new google.maps.LatLngBounds(
     new google.maps.LatLng(51.5009, -0.1926),
-    new google.maps.LatLng(51.5488, -0.2149)
+    new google.maps.LatLng(51.5131, -0.1589)
   );
 
   class CustomOverlay extends google.maps.OverlayView {
