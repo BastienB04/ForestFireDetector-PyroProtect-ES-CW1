638f15d2745175ca0bc7dd7efd8bbf45fef98c00
lucjones02 <lucjones@dyn3157-64.wlan.ic.ac.uk>
2023-02-16 20:36:58 +0000

    debug

--- server/index.js
+++ server/index.js
@@ -295,6 +295,7 @@ const server = http.createServer((req, res) => {
                 console.log(`index 304: ${tmp[0]}`);
                 console.log(`index 304: ${tmp[1]}`);
                 console.log(`index 304: ${tmp[2]}`);
+                console.log(stationHQ.getRadius());
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
                 res.write(JSON.stringify(HeatMap));
