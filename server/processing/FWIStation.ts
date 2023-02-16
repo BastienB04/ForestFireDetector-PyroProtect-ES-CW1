2b170235b3c57fcc93dd5f19e936c4e5b2744f1b
lucjones02 <lucjones@dyn3157-64.wlan.ic.ac.uk>
2023-02-16 14:16:37 +0000

    debug

--- server/processing/FWIStation.ts
+++ server/processing/FWIStation.ts
@@ -208,7 +208,8 @@ class FWIStation extends Station{
         else{
             b *= (1000/(25 + 108.64 * Math.exp(-0.023 * this.currentBUI)));
         }
-
+        
+        console.log(`b: ${b}`);
         if(b > 1){
             this.currentFWI = Math.exp(2.72 * Math.pow(0.434 * Math.log(b), 0.647));
             return this;
