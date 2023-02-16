d38de319d8d36be84f6c9f7c242c76349a8b95ad
lucjones02 <lucjones@dyn3157-64.wlan.ic.ac.uk>
2023-02-16 12:30:37 +0000

    debug

--- server/processing/FWIStation.ts
+++ server/processing/FWIStation.ts
@@ -49,6 +49,7 @@ class FWIStation extends Station{
         this.currentFFMC = 85;
         this.currentDMC = 6;
         this.currentDC = 15;
+        console.log(this.currentDC);
         console.log('finished constructing');
     }
 
