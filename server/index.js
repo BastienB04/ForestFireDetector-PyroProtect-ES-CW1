f283407da678224aef55d9e0672caf8e1b0207fe
lucjones02 <lucjones@dyn3157-64.wlan.ic.ac.uk>
2023-02-16 20:01:23 +0000

    sheeeesh

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const { StationBuilder } = require('./include/StationBuilder');
 const nodemailer = require('nodemailer');
 const { sendEmail } = require('./src/mail')
 const { spawn } = require('child_process');
-const kmToSquareRatio = 1/10;
+const kmToSquareRatio = 0.1;
 
 
 var sampleRate = 5;
