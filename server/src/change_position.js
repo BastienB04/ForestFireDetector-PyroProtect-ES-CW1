45c58356d6bbc4fc636f0bac60defdf1b87c837c
Zsombito <klapper.zsombor@gmail.com>
2023-02-16 02:15:35 +0000

    Update change_position.js

--- server/src/change_position.js
+++ server/src/change_position.js
@@ -4,7 +4,7 @@ const e = React.createElement;
 class PositionForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { x: '', y: '' };
+    this.state = { x: '', y: '' , d:''};
 
     this.handleChange1 = this.handleChange1.bind(this);
     this.handleChange2 = this.handleChange2.bind(this);
@@ -17,14 +17,16 @@ class PositionForm extends React.Component {
   handleChange2(event) {
     this.setState({y: event.target.value, x: this.state.x});
   }
-
+  handleChange3(event) {
+    this.setState({d : event.target.value});
+  }
   handleSubmit(event) {
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({x : this.state.x,
              y : this.state.y,
-              device:"device1"})
+              device:("device" + this.state.d)})
     };
     fetch('http://13.41.188.158:8080/api/ChangePosition', requestOptions);
     event.preventDefault();
@@ -38,7 +40,9 @@ class PositionForm extends React.Component {
           X:
           <input type="number" value={this.state.x} onChange={this.handleChange1} />
           Y:
-          <input type="number" value={this.state.y} onChange={this.handleChange2} />  
+          <input type="number" value={this.state.y} onChange={this.handleChange2} />
+          ID:
+          <input type="number" value={this.state.d} onChange={this.handleChange3} />
         </label>
         <input type="submit" value="Submit" />
       </form>
